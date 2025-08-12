b2af051caa613396aac399f58025a271958f58e6
HuzaifaFarooqSE <malikhuzaifa1309@gmail.com>
2025-08-12 22:23:31 +0000

    lesson donee

---
+++ 01_Basics/variable.js
@@ -0,0 +1,13 @@
+const accountId = 144225;
+let accountEmail = "Huzaifa@gmail.com"
+var accountPassword = 11223344;
+city = "Islamabad"
+let accountstate;
+console.table([accountId, accountEmail, accountPassword, city])
+
+accountEmail = "HuzaifaKhichi@gmail.com"
+accountPassword = 1234321;
+city = "Faislabad"
+
+console.table([accountId, accountEmail, accountPassword, city, accountstate])
+
