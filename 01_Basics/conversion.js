560dba1d18a4a344ccb0162fc376825b53770619
HuzaifaFarooqSE <malikhuzaifa1309@gmail.com>
2025-08-15 17:07:26 +0000

    Conversion Done

---
+++ 01_Basics/conversion.js
@@ -0,0 +1,24 @@
+// let score = "3!3";
+// let age = true;
+// let RealAGe = Number(age);
+
+// let valueInNumber = Number(score)
+// console.log(typeof score);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+// console.log(RealAGe)
+
+/* 
+   "33" When Converted is 33
+   while "33Huzaifa" (A string or a charecter added) will be NaN
+*/
+
+let isLoggedin = ""
+let BoolConversion = Boolean(isLoggedin);
+console.log(BoolConversion);
+
+/*
+Boolean Will be true when => (1, "Anything Inside the string")
+BOolean will be false when => (0, "")
+*/
+ 
